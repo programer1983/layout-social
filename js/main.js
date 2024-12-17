f27682bcc640f931199d8c326eade538ff168130
unknown <programer972@gmail.com>
2024-12-17 13:13:49 +0200

    create activity block

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 
-const container = document.querySelector('.container')
+const wrapper = document.querySelector('.wrapper')
 const button1 = document.querySelector('.header__button-light')
 const button2 = document.querySelector('.header__button-dark')
 const title = document.querySelector('.discover__title')
@@ -8,7 +8,7 @@ const nameUser = document.querySelector('.header__useer-name')
 
 button2.addEventListener('click', () => {
     if(button2){
-        container.classList.add('dark-theme')
+        wrapper.classList.add('dark-theme')
         button2.classList.add('border-line')
         button1.classList.remove('border-active')
         title.classList.add('color-title')
@@ -18,7 +18,7 @@ button2.addEventListener('click', () => {
 
 button1.addEventListener('click', () => {
     if(button1){
-        container.classList.remove('dark-theme')
+        wrapper.classList.remove('dark-theme')
         button2.classList.remove('border-line')
         button1.classList.add('border-active')
         title.classList.remove('color-title')
